32963f38127b4a36eb17e7f1c0294d54c5becb53
Xing Han Lu <xhlulu@users.noreply.github.com>
2017-07-14 14:15:08 -0400

    final

--- download_script.py
+++ download_script.py
@@ -1,9 +1,17 @@
+#!/usr/bin/env python
+
+"""
+Short Python Script that downloads all the free MSFT eBooks given away by Eric Ligman on July 14th 2017.
+LINK:
+https://blogs.msdn.microsoft.com/mssmallbiz/2017/07/11/largest-free-microsoft-ebook-giveaway-im-giving-away-millions-of-free-microsoft-ebooks-again-including-windows-10-office-365-office-2016-power-bi-azure-windows-8-1-office-2013-sharepo/?ranMID=24542&ranEAID=lw9MynSeamY&ranSiteID=lw9MynSeamY-I4Ne4ea4zo0cBxSms1JnBg&tduid=%282fa5d78bb69907999f4defb5aa79a64b%29%28256380%29%282459594%29%28lw9MynSeamY-I4Ne4ea4zo0cBxSms1JnBg%29%28%29
+"""
+
 import requests
 import os
 
 # Variables
 directory_name = "MSFT Books"
-list_links_url = 'http://ligman.me/2sZVmcG'
+links_list_url = 'http://ligman.me/2sZVmcG'  # URL that contains url of all the books
 print_status = True  # Print the current status of the script
 
 # Create a directory
@@ -12,18 +20,16 @@ if not os.path.exists(directory_name):
     if print_status:
         print("{} was successfully created.".format(directory_name))
 
-# Requesting the list of links
-response = requests.get(list_links_url)
-loop_ls = list(response.iter_lines(decode_unicode=True))
+initial_response = requests.get(links_list_url)  # Requesting the list of links
+loop = list(initial_response.iter_lines(decode_unicode=True))
+# Creates a list of all the links using the the iter_lines method of the Response class (from Requests)
 
-for line in loop_ls[1:3]:
-    url = line
+for url in loop[1:]:
+    response = requests.get(url)
+    book_name = response.url.split("/")[-1]  # Last part of the url is the title of the book
 
-    temp_response = requests.get(url)
-    book_name = temp_response.url.split("/")[-1]
-
-    with open('{}/{}'.format(directory_name, book_name), 'wb') as f:
-        f.write(temp_response.content)
+    with open('{}/{}'.format(directory_name, book_name), 'wb') as bin_file:  # Writing the file using open function
+        bin_file.write(response.content)
 
     if print_status:
-        print("{} was downloaded".format(book_name))
+        print("{} was downloaded.".format(book_name))
